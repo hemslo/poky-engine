4174e9b4ca2471a7008449933cd20ae6713b188c
Hemslo Wang <hemslo.wang@gmail.com>
2013-06-20 05:53:05 +0800

    Store links in mongodb
    Store document's url, links, text and body into mongodb.

--- poky_spider/poky_spider/pipelines.py
+++ poky_spider/poky_spider/pipelines.py
@@ -2,11 +2,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
+from pymongo import MongoClient
+import gridfs
 
 
 class PokySpiderPipeline(object):
+    db = MongoClient().poky
+    fs = gridfs.GridFS(db)
 
     def process_item(self, item, spider):
-        # store links (forward and reverse)
+        fid = self.fs.put(item['body'])
+        documents = self.db.documents
+        links = item['links']
+        linkids = []
+        for link in links:
+            document = documents.find_one({'url': link})
+            if document is None:
+                document = {'url': link}
+                document_id = documents.insert(document)
+            else:
+                document_id = document['_id']
+            linkids.append(document_id)
+
+        url = item["url"]
+        document = documents.find_one({'url': url})
+        if document is None:
+            document = {}
+        document['url'] = url
+        document['links'] = linkids
+        document['text'] = item['text']
+        document['body'] = fid
+        documents.save(document)
+
         # build reverse table
         return item
--- poky_spider/poky_spider/settings.py
+++ poky_spider/poky_spider/settings.py
@@ -13,6 +13,8 @@ NEWSPIDER_MODULE = 'poky_spider.spiders'
 DEPTH_PRIORITY = 1
 SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
 SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
-
+ITEM_PIPELINES = [
+    'poky_spider.pipelines.PokySpiderPipeline',
+]
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'poky_spider (+http://www.yourdomain.com)'
